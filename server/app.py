caafbde6893bf2efda20806c2556394c20f556aa
ITSmartHub <itsmarthubt@gmail.com>
2026-08-25 13:25:26 +0400

    Allow production frontend CORS

--- server/app.py
+++ server/app.py
@@ -23,11 +23,12 @@ app = FastAPI(title="FileNova Conversion API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://127.0.0.1:5174",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "https://filenova-web.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
