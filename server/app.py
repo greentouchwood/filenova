e4941c5c355f79881f11fb6781854dfbc27af81c
ITSmartHub <itsmarthubt@gmail.com>
2026-08-25 14:26:41 +0400

    Fix CORS for FileNova custom domain

--- server/app.py
+++ server/app.py
@@ -22,12 +22,14 @@ app = FastAPI(title="FileNova Conversion API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
+allow_origins=[
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5174",
     "http://127.0.0.1:5174",
     "https://filenova-web.onrender.com",
+    "https://filenova.online",
+    "https://www.filenova.online",
 ],
     allow_credentials=True,
     allow_methods=["*"],
